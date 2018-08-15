8fb5d013e061b717b0106b459dedb81c24f65f3b
JoMingyu <city7310@naver.com>
2018-08-16 00:27:39 +0900

    [UPDATE] 데코레이터를 반환하는 함수를 통해 중복 제거

--- flask_zipper/decorators.py
+++ flask_zipper/decorators.py
@@ -1,68 +1,43 @@
 from functools import wraps
 
-from flask import after_this_request, request
+from flask import after_this_request
 
 from flask_zipper.compressor import *
 from flask_zipper.exceptions import *
 from flask_zipper.util import get_zipper
 
 
-def encode_brotli(fn):
-    """
-    Brotli encoder
-    """
+def _get_decorator(kwargs_dict):
+    def decorator(fn):
+        @wraps(fn)
+        def wrapper(*args, **kwargs):
+            @after_this_request
+            def zipper(response):
+                kwargs_dict['response'] = response
 
-    @wraps(fn)
-    def wrapper(*args, **kwargs):
-        @after_this_request
-        def zipper(response):
-            return get_zipper().encode_response(
-                accept_encoding_string='br',
-                compressor=encode_response_with_brotli,
-                error_class=BrotliCompressionError,
-                response=response
-            )
+                return get_zipper().encode_response(**kwargs_dict)
 
-        return fn(*args, **kwargs)
-    return wrapper
+            return fn(*args, **kwargs)
+        return wrapper
+    return decorator
 
 
-def encode_deflate(fn):
-    """
-    Deflate(zlib) encoder
-    """
-
-    @wraps(fn)
-    def wrapper(*args, **kwargs):
-        @after_this_request
-        def zipper(response):
-            return get_zipper().encode_response(
-                accept_encoding_string='deflate',
-                compressor=encode_response_with_deflate,
-                error_class=DeflateCompressionError,
-                response=response
-            )
-
-        return fn(*args, **kwargs)
-    return wrapper
+encode_brotli = _get_decorator({
+    'accept_encoding_string': 'br',
+    'compressor': encode_response_with_brotli,
+    'error_class': BrotliCompressionError
+})
 
 
-def encode_gzipped(fn):
-    """
-    gzip encoder
-    """
+encode_deflate = _get_decorator({
+    'accept_encoding_string': 'deflate',
+    'compressor': encode_response_with_deflate,
+    'error_class': DeflateCompressionError
+})
 
-    @wraps(fn)
-    def wrapper(*args, **kwargs):
-        @after_this_request
-        def zipper(response):
-            return get_zipper().encode_response(
-                accept_encoding_string='gzip',
-                compressor=encode_response_with_gzip,
-                error_class=GzipCompressionError,
-                response=response
-            )
 
-        return fn(*args, **kwargs)
-
-    return wrapper
+encode_gzipped = _get_decorator({
+    'accept_encoding_string': 'gzip',
+    'compressor': encode_response_with_gzip,
+    'error_class': GzipCompressionError
+})
