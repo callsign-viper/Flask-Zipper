0c9710478bae0edacb7a2914dd37e93d32cfec30
JoMingyu <city7310@naver.com>
2018-08-16 22:54:11 +0900

    [DONE] content encoding 헤더, value가 없는 경우 plain response 테스트 추가, TestBrotli 완료

--- test.py
+++ test.py
@@ -9,6 +9,10 @@ from flask.testing import FlaskClient
 from flask_zipper import *
 
 
+def _decode_response_data_with_brotli(response):
+    return brotli.decompress(response.data).decode()
+
+
 class BaseTestCase(TestCase):
     def setUp(self):
         self.response_message = 'hello'
@@ -28,13 +32,40 @@ class BaseTestCase(TestCase):
 
         return app.test_client()
 
-    def _test_encoded_response(self, client, accept_encoding_string, decoder_function):
-        resp = client.get('/', headers={'Accept-Encoding': accept_encoding_string})
+    def _test_encoded_response(self, client, content_encoding_string, decoder_function):
+        resp = client.get('/', headers={'Accept-Encoding': content_encoding_string})
         self.assertEqual(resp.status_code, 200)
 
-        print(resp.headers)
-        self.assertEqual(resp.headers['Content-Encoding'], accept_encoding_string)
+        self.assertEqual(resp.headers['Content-Encoding'], content_encoding_string)
         self.assertEqual(resp.headers['Vary'], 'Accept-Encoding')
 
         self.assertEqual(decoder_function(resp), self.response_message)
 
+    def _test_content_encoding_header_missing(self, client):
+        resp = client.get('/')
+        self.assertEqual(resp.status_code, 200)
+
+        self.assertNotIn('Content-Encoding', resp.headers)
+
+        self.assertEqual(resp.data.decode(), self.response_message)
+
+    def _test_content_encoding_value_missing(self, client):
+        resp = client.get('/', headers={'Accept-Encoding': 'foo'})
+        self.assertEqual(resp.status_code, 200)
+
+        self.assertNotIn('Content-Encoding', resp.headers)
+
+        self.assertEqual(resp.data.decode(), self.response_message)
+
+
+class TestBrotli(BaseTestCase):
+    def setUp(self):
+        super(TestBrotli, self).setUp()
+        
+        self.target_func = encode_brotli
+        self.client = self._get_test_client_of_decorated_view_function_registered_flask_app(self.target_func)
+
+    def test(self):
+        self._test_encoded_response(self.client, 'br', _decode_response_data_with_brotli)
+        self._test_content_encoding_header_missing(self.client)
+        self._test_content_encoding_value_missing(self.client)
